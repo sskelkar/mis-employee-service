c8a5397a55e232cc113320fbf95bae38b20bef02
M.Gangurde <m.gangurde@maxxton.com>
2016-05-09 08:58:35 +0530

    Employee service update

--- src/main/java/com/maxxton/mis/employee/repository/EmployeeRepository.java
+++ src/main/java/com/maxxton/mis/employee/repository/EmployeeRepository.java
@@ -1,9 +1,16 @@
 package com.maxxton.mis.employee.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.maxxton.mis.employee.domain.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, Long>
+{
+
+//  @Query("select e from Employee e " + "connect by prior e.manager_id = e.employee_id " + "where e.employeeId = ?1 ")
+//  List<Employee> findCcEmailsForEmployee(Long employeeId);
 
 }
